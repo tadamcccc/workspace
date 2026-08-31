3b7326eecee31620f6b6f5622667919877f3f3f3
tadamcccc <tadam813@cougarmail.cccc.edu>
2026-08-31 14:36:04 -0400

    Add files via upload

---
+++ SongLyrics.java
@@ -0,0 +1,12 @@
+
+public class SongLyrics {
+
+	public static void main(String[] args) {
+		System.out.println("What will we do with a drunken sailor?");
+		System.out.println("What will we do with a drunken sailor?");
+		System.out.println("What will we do with a drunken sailor?");
+		System.out.println("Early in the morning");
+
+	}
+
+}
